85ebffe08615b389f73dffbdf48f5efdb157caab
Basel <baselkasim0@gmail.com>
2026-09-25 10:52:27 +0300

    second function

--- ProblemSolving.js
+++ ProblemSolving.js
@@ -1,5 +1,13 @@
-function printNumber(number){
-    console.log(number)
-
+function printNumber(number) {
+  console.log(number);
 }
-printNumber(30)
+printNumber(30);
+
+function divid(number) {
+  if (number % 3 == 0 && number % 4 == 0) {
+    console.log("right");
+  } else {
+    console.log("false");
+  }
+}
+divid(14);
